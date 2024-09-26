071aa9f0664ef32c885234c50aa5d36d382ebb5c
igor <igor70@yandex.ru>
2024-09-26 20:40:33 +0300

    initial

--- main.py
+++ main.py
@@ -1,11 +1,75 @@
 import configparser
+import requests
+import os
+import datetime
+import logging
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 # Получение данных из файла конфигурации
-token= config.get('Settings', 'token')
-password = config.get('Settings', 'period')
+TOKEN= config.get('Settings', 'token')
+local_dir = config.get('Settings', 'path_to_dir_local')
+yandex_dir = config.get('Settings', 'name_dir_disk_yandex')
+path_log = config.get('Settings', 'path_to_file_log')
 
-print(f'Token: {token}')
-print(f'Period: {password}')
+
+logging.basicConfig(level='INFO', format='%(asctime)s - %(levelname)s - %(message)s', filename=path_log)
+logger = logging.getLogger(__name__)
+
+
+
+URL = 'https://cloud-api.yandex.net/v1/disk/resources'
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': f'OAuth {TOKEN}'}
+
+class YandexDisk:
+    def __init__(self, token, yandex_dir, headers):
+        self.token = token
+        self.yandex_dir = yandex_dir
+        self.headers = headers
+
+    def get_dir(self):
+        """Получение списка файлов в указанной папке."""
+        response= requests.get(f"{URL}?path={self.yandex_dir}&fields=_embedded.items.name, _embedded.items.modified", headers=self.headers)
+        if response.status_code == 200:
+            res = response.json()
+            logger.info(f'Получение списка файлов в облачном хранилище в папке {self.yandex_dir}')
+            print(res['_embedded']['items'])
+            return res['_embedded']['items']
+        return
+
+    def upload_file(self, file, overwrite=False):
+        """Загрузка файла на Яндекс.Диск."""
+        response = requests.get(f'{URL}/upload?path={self.yandex_dir}/{file}&overwrite={overwrite}', headers=headers)
+        if response.status_code == 200:
+            link = response.json()['href']
+            print(link)
+            with open(file, 'rb') as f:
+                files = {'file': f}
+                res = requests.put(link, files=files)
+                if res.status_code == 201:
+                    logger.info(f"Файл {file} успешно загружен на Яндекс.Диск")
+                else:
+                    return f'Ошибка загрузки файла на Яндекс.Диск {res.status_code}'
+
+        else:
+            return f'Ошибка загрузки файла на Яндекс.Диск {response.status_code}'
+
+    def delete_file(self, file):
+        """Удаление файла с Яндекс.Диска."""
+        response = requests.delete(f'{URL}?path={self.yandex_dir}/{file}', headers=headers)
+        if response.status_code == 204:
+            logger.info(f"Файл {file} успешно удален с Яндекс.Диска")
+        else:
+            return f'Ошибка удаления файла с Яндекс.Диска {response.status_code}'
+
+
+if __name__ == '__main__':
+
+    print(os. listdir())
+
+    #a = YandexDisk(token=TOKEN, yandex_dir=yandex_dir, headers=headers)
+    #a.get_dir()
+    #a.upload_file(file='test.txt')
+    #a.delete_file(file='test.txt')
--- setup_config.py
+++ setup_config.py
@@ -4,9 +4,9 @@ config = configparser.ConfigParser()
 config.add_section('Settings')
 config.set('Settings', 'token', 'y0_AgAAAAAAR7pHAADLWwAAAAESCLNkAACns3-yza9HC46dGL72sSdeeAgPUw')
 config.set('Settings', 'path_to_dir_local', '/home/igor/Документы')
-config.set('Settings', 'name_dir_disk_yandex', 'Документы_Игорь')
+config.set('Settings', 'name_dir_disk_yandex', 'temp')
 config.set('Settings', 'period', '10')
-config.set('Settings', 'path_to_file_log', '/home/igor/log.log')
+config.set('Settings', 'path_to_file_log', 'log.log')
 
 # Сохранение конфигурации в файл
 with open('config.ini', 'w') as config_file:
